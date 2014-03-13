55fcb22c1f95890bae74999d955a67743f274498
her0satr <her0satr@yahoo.com>
2014-03-13 16:25:53 +0700

    update

--- sygaas_db.sql
+++ sygaas_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 13, 2014 at 04:18 PM
+-- Generation Time: Mar 13, 2014 at 04:25 PM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.19
 
